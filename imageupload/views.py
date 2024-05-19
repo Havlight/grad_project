629be2f809af7ac06d82ca14d48ccadddf29ee36
Havlight <a24961782@gmail.com>
2024-05-19 21:28:53 +0800

    4th

--- imageupload/views.py
+++ imageupload/views.py
@@ -1,10 +1,12 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Image
 from .serializers import ImageSerializer
 
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def upload_image(request):
     serializer = ImageSerializer(data=request.data)
     if serializer.is_valid():
